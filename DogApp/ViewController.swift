86ed942fdc0de51070411a18e42d54421bf26fde
Wilfred Anorma <wilfred@ambiclimate.com>
2018-11-25 16:05:34 +0800

    Initial Commit

---
+++ DogApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DogApp
+//
+//  Created by Wilfred Anorma on 25/11/2018.
+//  Copyright © 2018 Wilfred Anorma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
